ac015cb8e1ebd90fd9b7b8b18786162c7c986aa8
Dmitriy Dubson <d.dubson@gmail.com>
2016-07-08 18:30:58 -0400

    Added some basic logic

--- omni.go
+++ omni.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to Omni")
+	if len(os.Args) < 2 {
+		fmt.Println("Specify a command yo!")
+		return
+	}
+
+	command := os.Args[1]
+
+	if command == "commands" {
+		fmt.Println("Supported commands:")
+		fmt.Println("add-to-path")
+	} else if command == "add-to-path" {
+		fmt.Println("let's add something to the PATH variable")
+	} else {
+		fmt.Println("No such command. Run 'commands' to view what's available")
+	}
 }
